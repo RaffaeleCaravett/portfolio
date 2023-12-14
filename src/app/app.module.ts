55067daddb1fc538cc740d131b78b2452273dfce
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-14 16:47:47 +0100

    preparing to push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ContattiComponent } from './Components/contatti/contatti.component';
+import { AboutMeComponent } from './Components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContattiComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
